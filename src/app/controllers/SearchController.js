9383f0638dd127ce4c304d209e125baeb45bf312
gustavo-sorati <gustavo.sorati@outlook.com>
2021-04-20 22:11:20 -0300

    inicio desafio 7 - Envio de imagens foodfy

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -1,30 +1,34 @@
 const { findByChefId } = require('../models/Recipe');
 const Chef = require('../models/Chef');
 const Recipe = require('../models/Recipe');
+const path = require('path');
 
 module.exports = {
   async search(req, res) {
     try {
       let { filter } = req.query;
 
-      //   if (!filter) return res.redirect('/recipes');
+      if (!filter) return res.redirect('/recipes');
 
       let results = await Recipe.search(filter);
 
+      
       const recipesPromisse = results.rows.map(async (recipe) => {
-        recipe.author = await (await Chef.findById(recipe.chef_id)).rows[0]
-          .name;
-
+        recipe.chef_name = await (await Chef.findById(recipe.chef_id)).rows[0]
+        .name;
+        
         return recipe;
       });
+      
+      const recipes = await Promise.all(recipesPromisse);
 
-      let recipes = await Promise.all(recipesPromisse);
+      if(recipes.length === 0) return res.redirect('/recipes');
 
       let search = {
         filter,
       };
 
-      return res.render('common/recipes/index', { recipes, search });
+      return res.render(`public/recipes/index`, { recipes, search });
     } catch (err) {
       console.log(err);
     }
